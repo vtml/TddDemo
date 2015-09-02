0f8a206e3c9670ad09004cff7d909329a27e9958
Vincent Lui <vincent.lui@tpg.com.au>
2015-09-02 23:28:10 +1000

    Data Models

---
+++ TddDemo.Framework/Models/Airport.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TddDemo.Framework.Models
+{
+    public partial interface IAirport
+    {
+        Guid Id { get; set; }
+        
+        string AirportCode { get; set; }
+
+        string AirportName { get; set; }
+
+        string Latitude { get; set; }
+
+        string Longitude { get; set; }
+    }
+
+    public partial class Airport : IAirport
+    {
+        public Guid Id { get; set; }        
+        public virtual string AirportCode { get; set; }
+        public virtual string AirportName { get; set; }
+        public virtual IEnumerable<Guid> City { get; set; }
+        public virtual string Latitude { get; set; }
+        public virtual string Longitude { get; set; }
+    }
+}
---
+++ TddDemo.Framework/Models/DataModels/EntryDataModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddDemo.Framework.Models.DataModels
+{
+    public class EntryDataModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Airport Airport { get; set; }
+
+        public int NumberOfPets { get; set; }
+    }
+}
