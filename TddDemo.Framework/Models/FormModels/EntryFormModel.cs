cba87f6a2020424806fca676b133145016a58004
vlui <vincent.lui@isobar.com>
2015-09-02 20:13:25 +1000

    FormModel unit test

---
+++ TddDemo.Framework/Models/FormModels/EntryFormModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TddDemo.Framework.Models.FormModels
+{
+    public class EntryFormModel
+    {
+        [Required]
+        [MinLength(1)]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(3)]
+        public string AirportCode { get; set; }
+
+        [Range(0, 100)]
+        public int NumberOfPets { get; set; }
+    }
+}
---
+++ TddDemo.UnitTest/Models/FormModels/EntryFormModelTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Kernel;
+using TddDemo.Framework.Models.FormModels;
+
+namespace TddDemo.UnitTest.Models.FormModels
+{
+    public class EntryFormModelTest
+    {
+        private static readonly Fixture Fixture = new Fixture();
+        
+        private static object[] VariousFormModels()
+        {
+            var validAirportCode = Fixture.Create<string>().Substring(0, 3);
+            var validFormModel = Fixture.Build<EntryFormModel>().With(x => x.AirportCode, validAirportCode).With(x => x.NumberOfPets, new Generator<int>(Fixture).First(pt => pt > 0 && pt < 100)).Create();
+            var noFirstName = Fixture.Build<EntryFormModel>().With(x => x.AirportCode, validAirportCode).Without(x => x.FirstName).Create();
+            var noLastName = Fixture.Build<EntryFormModel>().With(x => x.AirportCode, validAirportCode).Without(x => x.LastName).Create();
+            var lotsOfPets = Fixture.Build<EntryFormModel>().With(x => x.AirportCode, validAirportCode).With(x => x.NumberOfPets, new Generator<int>(Fixture).First(pt => pt >  100 && pt <  500)).Create();
+            var list = new object[]
+                    {
+                        new object[] {validFormModel, true},
+                        new object[] {noFirstName, false},
+                        new object[] {noLastName, false},
+                        new object[] {lotsOfPets, false},
+                    };
+            return list;    
+        }
+
+        [Test, TestCaseSource("VariousFormModels")]
+        public void EntryFormModel_DataAnnotationTest(EntryFormModel model, bool isValid)
+        {
+            var validationResultsList = new List<ValidationResult>();
+            var validationContext = new ValidationContext(model, null, null);
+            var validaitonResult = Validator.TryValidateObject(model, validationContext, validationResultsList, true);
+
+            Assert.AreEqual(isValid, validaitonResult);
+            Assert.AreEqual(isValid, !validationResultsList.Any());
+        }
+    }
+}
