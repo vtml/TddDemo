59e2e695286007110775d95d26b04f55fcd41cd3
vlui <vincent.lui@isobar.com>
2015-09-03 20:14:18 +1000

    Checking whether the repository add command was called

--- TddDemo.Web.UnitTest/Controller/EntryControllerTest.cs
+++ TddDemo.Web.UnitTest/Controller/EntryControllerTest.cs
@@ -2,6 +2,7 @@
 using NSubstitute;
 using NUnit.Framework;
 using Ploeh.AutoFixture;
+using TddDemo.Framework.Data;
 using TddDemo.Framework.Models;
 using TddDemo.Framework.Models.DataModels;
 using TddDemo.Framework.Models.FormModels;
@@ -13,6 +14,7 @@ namespace TddDemo.Web.UnitTest.Controller
     public class EntryControllerTest
     {
         private readonly IMapper<EntryFormModel, EntryDataModel> _mapper = Substitute.For<IMapper<EntryFormModel, EntryDataModel>>();
+
         private static readonly Fixture Fixture = new Fixture();
 
         [TestFixtureSetUp]
@@ -24,45 +26,52 @@ namespace TddDemo.Web.UnitTest.Controller
         [Test]
         public void Create_ShouldRedirectToIndex_WhenModelIsNotValid()
         {
-            var controller = new EntryController(_mapper);
+            var entryDataModelRepository = Substitute.For<IRepository<EntryDataModel>>();
+            var controller = new EntryController(_mapper, entryDataModelRepository);
             controller.ModelState.AddModelError(Fixture.Create<string>(), Fixture.Create<string>());
 
             var result = controller.Create(Fixture.Create<EntryFormModel>()) as RedirectToRouteResult;
 
             Assert.IsFalse(controller.ModelState.IsValid);
             Assert.IsNotNull(result);
+            entryDataModelRepository.DidNotReceiveWithAnyArgs().Add(Arg.Any<EntryDataModel>());
             StringAssert.AreEqualIgnoringCase("Index", result.RouteValues["action"].ToString());
         }
 
         [Test]
         public void Create_ShouldRedirectToIndex_WhenAirportIsNull()
         {
+            var entryDataModelRepository = Substitute.For<IRepository<EntryDataModel>>();
             var mapper = Substitute.For<IMapper<EntryFormModel, EntryDataModel>>();
             mapper.Map(Arg.Any<EntryFormModel>()).Returns(Fixture.Build<EntryDataModel>().With(x => x.Airport, null).Create());
 
-            var controller = new EntryController(mapper);
+            var controller = new EntryController(mapper, entryDataModelRepository);
             controller.ModelState.Clear();
 
             var result = controller.Create(Fixture.Create<EntryFormModel>()) as RedirectToRouteResult;
 
             Assert.IsTrue(controller.ModelState.IsValid);
             Assert.IsNotNull(result);
+            entryDataModelRepository.DidNotReceiveWithAnyArgs().Add(Arg.Any<EntryDataModel>());
             StringAssert.AreEqualIgnoringCase("Index", result.RouteValues["action"].ToString());
         }
 
         [Test]
         public void Create_ShouldRedirectToDetail_WhenModelIsValidAndAirportIsNotNull()
         {
+            var entryDataModelRepository = Substitute.For<IRepository<EntryDataModel>>();
+            var entryDataModel = Fixture.Create<EntryDataModel>();
             var mapper = Substitute.For<IMapper<EntryFormModel, EntryDataModel>>();
-            mapper.Map(Arg.Any<EntryFormModel>()).Returns(Fixture.Create<EntryDataModel>());
+            mapper.Map(Arg.Any<EntryFormModel>()).Returns(entryDataModel);
 
-            var controller = new EntryController(mapper);
+            var controller = new EntryController(mapper, entryDataModelRepository);
             controller.ModelState.Clear();
 
             var result = controller.Create(Fixture.Create<EntryFormModel>()) as RedirectToRouteResult;
 
             Assert.IsTrue(controller.ModelState.IsValid);
             Assert.IsNotNull(result);
+            entryDataModelRepository.Received().Add(entryDataModel);
             StringAssert.AreEqualIgnoringCase("Detail", result.RouteValues["action"].ToString());
         }
     }
--- TddDemo.Web/Controllers/EntryController.cs
+++ TddDemo.Web/Controllers/EntryController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using TddDemo.Framework.Data;
 using TddDemo.Framework.Models.DataModels;
 using TddDemo.Framework.Models.FormModels;
 using TddDemo.Framework.Models.Mappers;
@@ -8,10 +9,12 @@ namespace TddDemo.Web.Controllers
     public class EntryController : Controller
     {
         private readonly IMapper<EntryFormModel, EntryDataModel> _mapper;
+        private readonly IRepository<EntryDataModel> _entryDataModelRepository;
 
-        public EntryController(IMapper<EntryFormModel, EntryDataModel> mapper)
+        public EntryController(IMapper<EntryFormModel, EntryDataModel> mapper, IRepository<EntryDataModel> entryDataModelRepository)
         {
             _mapper = mapper;
+            _entryDataModelRepository = entryDataModelRepository;
         }
 
         public ActionResult Index()
@@ -25,7 +28,9 @@ namespace TddDemo.Web.Controllers
         {
             if (!ModelState.IsValid) return RedirectToAction("Index");
             var entryDataModel = _mapper.Map(entry);
-            return entryDataModel.Airport != null ? RedirectToAction("Detail", new {entry = entryDataModel}) : RedirectToAction("Index");
+            if (entryDataModel.Airport == null) return RedirectToAction("Index");
+            _entryDataModelRepository.Add(entryDataModel);
+            return RedirectToAction("Detail", new {entry = entryDataModel});
         }
 
         public ActionResult Detail(EntryDataModel entry)
